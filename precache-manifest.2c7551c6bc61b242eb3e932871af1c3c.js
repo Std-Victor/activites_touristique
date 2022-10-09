eaceaa3a527eb39f6bf3438cb8f258b8a7ee738a
abc <abc@example.com>
2022-10-10 00:12:32 +0100

    Updates

---
+++ precache-manifest.2c7551c6bc61b242eb3e932871af1c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b393894f2afbdd0ea4",
+    "url": "/activites_touristique/static/css/main.c7b8b3da.chunk.css"
+  },
+  {
+    "revision": "77b393894f2afbdd0ea4",
+    "url": "/activites_touristique/static/js/main.77b39389.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/activites_touristique/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "7372631245bdf377fbdf",
+    "url": "/activites_touristique/static/js/2.73726312.chunk.js"
+  },
+  {
+    "revision": "5f8ae9cafe607b9f0d4c",
+    "url": "/activites_touristique/static/js/runtime~main.5f8ae9ca.js"
+  },
+  {
+    "revision": "c96eb7e7d92816eed25204cc26c6292d",
+    "url": "/activites_touristique/index.html"
+  }
+];
